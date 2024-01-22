5c6b5c48b64171a64e12d4d78c68de9d28a77b12
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-01-22 15:24:30 +0900

    Feat: 비동기 처리 개선 및 로딩 표시 추가

--- layouts/main/article/projects/projects.js
+++ layouts/main/article/projects/projects.js
@@ -1,36 +1,47 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // API 엔드포인트와 초기 페이지 및 이미지 수 설정
     let apiUrl = "https://picsum.photos/v2/list";
-    let currentPage = 1;
+    let currentPage = 2;
     let limit = 3;
 
-    // 함수를 통해 API에서 이미지 가져오기
-    function fetchImages() {
-        fetch(`${apiUrl}?page=${currentPage}&limit=${limit}`)
-            .then(response => response.json())
-            .then(data => {
-                // 가져온 데이터를 이용하여 이미지 동적으로 추가
-                const apiImgSection = document.querySelector(".projects-api");
+    async function fetchImages() {
+        try {
+            const response = await fetch(`${apiUrl}?page=${currentPage}&limit=${limit}`);
+            const data = await response.json();
 
-                data.forEach(item => {
-                    const img = document.createElement("img");
-                    img.src = item.download_url;
-                    img.alt = "Random Image";
-                    apiImgSection.appendChild(img);
-                });
-            })
-            .catch(error => console.error("Error fetching images:", error));
+            const apiImgSection = document.querySelector(".projects-api");
+
+            data.forEach(item => {
+                const img = document.createElement("img");
+                img.src = item.download_url;
+                img.alt = "Random Image";
+                apiImgSection.appendChild(img);
+            });
+        } catch (error) {
+            console.error("Error fetching images:", error);
+        }
     }
 
-    // 초기 로딩 시 이미지 가져오기
-    fetchImages();
+    async function preloadImages() {
+        const response = await fetch(`${apiUrl}?page=${currentPage}&limit=${limit}`);
+        const data = await response.json();
 
-    // 더보기 버튼 클릭 시 다음 페이지의 이미지 가져오기
+        data.forEach(item => {
+            const img = new Image();
+            img.src = item.download_url;
+        });
+    }
+
+    preloadImages();
+
     const showMoreBtn = document.querySelector(".show-more-btn");
+
     showMoreBtn.addEventListener("click", function () {
-        // 다음 페이지 설정
+        showMoreBtn.textContent = "Loading...";
+
         currentPage++;
-        // 이미지 추가
-        fetchImages();
+
+        fetchImages().then(() => {
+            showMoreBtn.textContent = "Show more";
+        });
     });
 });
