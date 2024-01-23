845014a4ac0a8e14f416eb6490327a88223acc81
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-01-23 21:14:32 +0900

    Feat: 지도 우측 상단 오버레이 재생성

--- layouts/main/article/map/map.js
+++ layouts/main/article/map/map.js
@@ -8,6 +8,17 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 // 지도를 생성한다
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
+// 지도 타입 변경 컨트롤을 생성한다
+var mapTypeControl = new kakao.maps.MapTypeControl();
+
+// 지도의 상단 우측에 지도 타입 변경 컨트롤을 추가한다
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+// 지도에 확대 축소 컨트롤을 생성한다
+var zoomControl = new kakao.maps.ZoomControl();
+
+// 지도의 우측에 확대 축소 컨트롤을 추가한다
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
 // 커스텀 오버레이를 생성하고 지도에 표시한다
 var customOverlay = new kakao.maps.CustomOverlay({
